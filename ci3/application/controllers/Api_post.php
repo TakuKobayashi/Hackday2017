1013c6b385f9946da710f7189274e0d1610108d9
eye <eyepyon@gmail.com>
2017-02-05 07:22:33 +0900

    test

--- ci3/application/controllers/Api_post.php
+++ ci3/application/controllers/Api_post.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: eye
  * Date: 2017/02/04
  * Time: 22:55
  */
-
 class Api_post extends MY_Controller
 {
 
@@ -40,5 +40,60 @@ class Api_post extends MY_Controller
             $this->load->view('upload_success', $data);
         }
     }
+
+    function trtest()
+    {
+        $azure_key = "80ff081d32654875b59d1a63e6ada561";  // !!! TODO: secret key here !!!
+        $fromLanguage = "en";  // Translator Language Codes: https://msdn.microsoft.com/en-us/library/hh456380.aspx
+        $toLanguage = "ja";
+        $inputStr = "AZURE - The official documentation and examples for PHP are useless.";
+
+// Get the translation
+        $accessToken = $this->getToken($azure_key);
+        $params = "text=" . urlencode($inputStr) . "&to=" . $toLanguage . "&from=" . $fromLanguage . "&appId=Bearer+" . $accessToken;
+        $translateUrl = "http://api.microsofttranslator.com/v2/Http.svc/Translate?$params";
+        $curlResponse = $this->curlRequest($translateUrl);
+        $translatedStr = simplexml_load_string($curlResponse);
+// Display the translated text on the web page:
+        echo "<p>From " . $fromLanguage . ": " . $inputStr . "<br>";
+        echo "To " . $toLanguage . ": " . $translatedStr . "<br>";
+        echo date("r") . "</p>";
+
+    }
+// and leave the rest of the code as it is ;-)
+// Get the AZURE token
+    function getToken($azure_key)
+    {
+        $url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken';
+        $ch = curl_init();
+        $data_string = json_encode('{body}');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data_string),
+                'Ocp-Apim-Subscription-Key: ' . $azure_key
+            )
+        );
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $strResponse = curl_exec($ch);
+        curl_close($ch);
+        return $strResponse;
+    }
+
+// Request the translation
+    function curlRequest($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, "Content-Type: text/xml");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $curlResponse = curl_exec($ch);
+        curl_close($ch);
+        return $curlResponse;
+    }
+
 
 }
