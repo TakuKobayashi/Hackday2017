3367e721061e9ddd2b67dd4b7707e5ab8efaad1d
eye <eyepyon@gmail.com>
2017-02-05 10:20:27 +0900

    test

--- ci3/application/controllers/Api_post.php
+++ ci3/application/controllers/Api_post.php
@@ -25,7 +25,7 @@ class Api_post extends MY_Controller
 
     function conv(){
         $key = 'AIzaSyDRC0-wdBWTd3-OB122Vx9pXFB8SXs0ifY';
-       $res = sprintf('https://www.googleapis.com/language/translate/v2?key=%s&q=hello%20world&source=en&target=ja',$key);
+       $res = sprintf('https://www.googleapis.com/language/translate/v2?key=%s&q=hello&source=en&target=ja',$key);
         print_r($res);
     }
 
